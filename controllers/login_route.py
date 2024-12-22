b5fcbf44e87ba33f6fe3826d4a901fdb7e6bd1ed
Gaurish17Sharma <gaurishsharma03@gmail.com>
2024-12-22 17:46:29 +0530

    login controller

--- controllers/login_route.py
+++ controllers/login_route.py
@@ -2,7 +2,7 @@ from models import Users , db
 from werkzeug.security import generate_password_hash , check_password_hash
 from flask import Flask , render_template , flash , redirect , url_for
 from forms import Loginform
-from flask_login import LoginManager , UserMixin , login_user , login_required , logout_user , current_user
+from flask_login import login_user , login_required , logout_user , current_user
 
 #login form
 def loginform():
